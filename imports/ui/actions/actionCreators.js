91b11d6d6c9fde21d6ad498fc13071e42cb72abc
Mark Hurwitz <mhurwitzus@gmail.com>
2016-05-30 06:38:13 -0500

    Refactor accounts related actions to use thunks

--- imports/ui/actions/actionCreators.js
+++ imports/ui/actions/actionCreators.js
@@ -1,21 +1,52 @@
 import actionTypes from './actionTypes';
+import { Accounts } from 'meteor/accounts-base';
+import Alert from 'react-s-alert';
+import { browserHistory } from 'react-router'
 
-export function recoverPassword(email) {
-  console.log('actionCreators: recoverPassword');
+
+export function recoverPasswordSuccess(email) {
   return {
-    type: actionTypes.RECOVER_PASSWORD,
+    type: actionTypes.RECOVER_PASSWORD_SUCCESS,
     email
   };
 }
 
-
-export function resetPassword(token, password) {
-  console.log('actionCreators: resetPassword');
+export function resetPasswordSuccess(token, password) {
   return {
-    type: actionTypes.RESET_PASSWORD,
+    type: actionTypes.RESET_PASSWORD_SUCCESS,
     token,
     password
   };
+}
+
+export function recoverPassword(email) {
+  return function(dispatch) {
+    console.log('actionCreators: recoverPassword');
+    Accounts.forgotPassword({email: email}, (err) => {
+      if (err) {
+        if (err.reason) Alert.warning(err.reason);
+        console.log(err);
+      }
+      Alert.success('An email has been sent.');
+      dispatch(recoverPasswordSuccess(email));
+    });
+  }
+}
+
+export function resetPassword(token, password) {
+  console.log('actionCreators: resetPassword');
+  return function(dispatch) {
+    Accounts.resetPassword(token, password, (err) => {
+      if (err) {
+        if (err.reason) Alert.warning(err.reason);
+        console.log(err);
+      }
+
+      Alert.success('Your password has been reset.');
+      browserHistory.push('/')
+      dispatch(resetPasswordSuccess(token, password))
+    });
+  }
 }
 
 export function createWidgetSuccess(data) {
--- imports/ui/reducers/accounts.js
+++ imports/ui/reducers/accounts.js
@@ -1,36 +1,13 @@
-import { Accounts } from 'meteor/accounts-base';
 import actionTypes from '../actions/actionTypes';
-import Alert from 'react-s-alert';
-import { browserHistory } from 'react-router'
 
 export default function accounts(state = {}, action) {
   console.log('reducers/accounts')
   switch (action.type) {
-    case actionTypes.RECOVER_PASSWORD:
-      console.log('RECOVER_PASSWORD');
-      Accounts.forgotPassword({email: action.email}, (err) => {
-        if (err) {
-          if (err.reason) Alert.warning(err.reason);
-          console.log(err);
-        }
-        Alert.success('An email has been sent.');
-        console.log('Accounts.forgotPassword: callback called without error');
-      });
+    case actionTypes.RECOVER_PASSWORD_SUCCESS:
+      console.log('RECOVER_PASSWORD_SUCCESS');
       return state;
-    case actionTypes.RESET_PASSWORD:
-      console.log('RESET_PASSWORD');
-
-      Accounts.resetPassword(action.token, action.password, (err) => {
-        if (err) {
-          if (err.reason) Alert.warning(err.reason);
-          console.log(err);
-        }
-
-        Alert.success('Your password has been reset.');
-        browserHistory.push('/')
-        console.log('Accounts.resetPassword: callback called without error');
-      });
-
+    case actionTypes.RESET_PASSWORD_SUCCESS:
+      console.log('RESET_PASSWORD_SUCCESS');
       return state;
     default:
       return state;
